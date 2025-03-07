1b70ec69d8b0c85e7b95b6645aca40bea32b84f0
pavan-rajesh <pavanrajesh@zithara.in>
2025-03-07 11:18:53 +0530

    minor changes

--- src/app/components/home-page/BGBody.tsx
+++ src/app/components/home-page/BGBody.tsx
@@ -4,7 +4,7 @@ import React from 'react'
 const BGBody = () => {
     return (
         <div className='min-h-screen bg-black'>
-            <Image src='/wallpaper.jpg' fill="true" alt='wallpaper' objectFit='cover' />
+            <Image src='/wallpaper.jpg' fill={true} alt='wallpaper' objectFit='cover' />
         </div>
     )
 }
